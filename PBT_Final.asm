24a4c6b2911362b6c88494ca914f15b5be2c4881
Domenic <domperriccioli@hotmail.com>
2017-03-17 14:19:15 -0400

    Add files via upload

--- PBT_Final.asm
+++ PBT_Final.asm
@@ -1,4 +1,15 @@
-;common dec
+;Astro Clock
+;Domenic,Martin,Paul,Chris^2
+;March 17 2017
+;Phys 2P32
+;The task is to detect (count) the time of an astronomical Event, with 10ms resolution (assuming a precise clock of 100 Hz is available externally, if necessary),
+;and to display the time in the format "YY:DDD:HH:MM:SS.mm", where YY=0..99, DDD=0..365 (note that one year is 365.25 days), HH=0..23, MM=0..59, SS=0..59, mm=0..99. ;Assume you would be using the LCD display, 2x40 characters.
+;
+;To test transitions across hour/day/year boundaries your counter should be pre-loadable with appropriate values that can demonstrate these transitions within a few ;seconds, e.g. something like "00:365:23:59:55.00" could be pre-loaded as a starting point for the "stopwatch". 
+;Uses LCD subroutines to display to LCD Display
+;Uses built in Math subroutines
+
+;common declarations
 Milisecs equ	0x20
 Days0	equ	0x21
 Days1	equ	0x22
@@ -23,85 +34,95 @@ leapYr	equ	0x25
 	movwf	Year1 	;initilize Year0
 	movlw	17
 	movwf	Year0 	;initilize Year1	
-
+	call    testLeap
 	call LCDinit
 
 begin	
-	movlw	1
+	movlw	1		;adds 1 second to delay
 	call 	Delay1s
     
-	movlw   0x00
+	movlw   0x00		;sets lcd pointer to top left corner
 	call    LCDset    
     
-	call    DisplayYear
+	call    DisplayYear	;displays year (Y1/y0)
 
-	movlw   ':
+	movlw   ':		;adds a colon
 	call    ASC2LCD
     
 
-	call    DisplayDay
+	call    DisplayDay	;displays days
     
-	movlw	0x40
+	movlw	0x40		;moves lcd pointer to bottom left corner
 	call	LCDset
 
-	movlw   ':
+	movlw   ':		
 	call    ASC2LCD
-
-	call    RTC2LCD
+			
+	call    RTC2LCD		;displays real time clock (HH:MM:SS)
      
-	call    testLeap
-	call	incDaysYears
-	
-	call	Getkey
-	
-	call 	TxByte
+	call	incDaysYears	
 
-	sublw	7
-	btfsc	STATUS,Z
-	goto	begin
-	movlw	'.
-	call	ASC2LCD
-	movf	Count5ms,W
-	movwf	WL
-	movlw	5
-	call	Mul8x8
-	movlw	100
-	movwf	CH
-	call	Div16x8
-	movwf	temp
-	movf	WL,W
-	addlw	48
-	call	ASC2LCD
-	movf	temp,W
-	clrf	WH
-	movwf	WL
-	movlw	10
-	movwf	CH
-	call	Div16x8
-	movwf	temp
-	movf	WL,W
-	addlw	48
-	call	ASC2LCD
-	movf	temp,W
-	addlw	48
-	call	ASC2LCD
+;;;;;;;;;;;;;;;;;;;;;;
+;allows for button to stop program and display count for ms (x/1000)
+;;;;;;;;;;;;;;;;;;;;
+	
+	call	Getkey		;allows for a button to stop program and display MS count
+
+	sublw	7		;subtracts literally 7
+	btfsc	STATUS,Z	;test to see if button was pressed
+	goto	begin		;if not go back
+	movlw	'.		;add decimale for ms display
+	call	ASC2LCD		;displays decimal
+	movf	Count5ms,W	;moves 5ms count into W
+	movwf	WL		;moves into WL
+	movlw	5		;bring literal 5 to W
+	call	Mul8x8		;multiply 5 ms count by 5 to create ms
+	movlw	100		;move 100 to W
+	movwf	CH		;put it into CH to divide
+	call	Div16x8	;divide the miliseconds by 100
+	movwf	temp		;put remainder into temp
+	movf	WL,W		;move result into W
+	addlw	48		;add 48 to convert into ASCII
+	call	ASC2LCD	;write the char to LCD
+	movf	temp,W		;put remainder back into W
+	clrf	WH		;clear the WH
+	movwf	WL		;put remainder back into lower one
+	movlw	10		;move 10 to CH 
+	movwf	CH		;to divide by 10
+	call	Div16x8	;divide remainder by 10 to get last two digits
+	movwf	temp		;move remainder to temp
+	movf	WL,W		;move result to W
+	addlw	48		;add 48 to convert to ASCII
+	call	ASC2LCD	;write to LCD
+	movf	temp,W		;move remainder to W
+	addlw	48		;add 48 to convert to ASCII
+	call	ASC2LCD	;write to LCD
 	return	
+;;;;;;;;;;;;;;
+;Main End Here
+;;;;;;;;;;;;;;
 
 nextYear
 ;Martin
 ;count up the year and clears days 
 ;year1 = century (00-99)
 ;year0 = year within the century (00-99)
-	clrf	Days0
-	clrf	Days1
-	incf	Year0
-	movlw	100
-	subwf	Year0
-	btfss	STATUS,Z
-	return
-	clrf	Year0
-	incf	Year1
-	return
+	clrf	Days0		;clear days (two least significant digits) register (00-255) 
+	clrf	Days1		;clear days (the most significant digit) register(255-364/5)
+	
+	incf	Year0		;increments year0 register (decades 00-99)
+	call    testLeap	;checks for leap year
+	movlw	100		;putting the year limit value into w (100)
+	subwf	Year0,W		;;subtract the value in year0 from w
+	btfss	STATUS,Z	;test for year 100
+	return			;if no flag then keep counting
+	clrf	Year0		;if at 100 clears decades (17-99)
+	incf	Year1		;increments centuries/milleniums (20-99)
+	movlw	100		;move literal 100 into W
+	subwf	Year1,W		;subtract C/M-100
+	btfsc	STATUS,Z	;test for year 9999
+	clrf	Year1		;no way
+	return			;cant be true
 
 
 DisplayYear
@@ -148,14 +169,14 @@ DisplayYear
 DisplayDay
 ;displays the day as a 3 digit number
 	
-	movf	Days0,W
+	movf	Days0,W	;put first half of days into WL
 	movwf	WL
-	movf	Days1,W
+	movf	Days1,W	;put second half od days into WH
 	movwf	WH
 
 	movlw	100
-	movwf	CH
-	call	Div16x8	;
+	movwf	CH	;put 100 into CH to divide by 100
+	call	Div16x8	;divide days by 100
 	movwf	temp	;put remainder in temp
 	movf	WL,W	;put result in W
 	addlw	48	;add 30 to result to convert to ASCII
@@ -164,8 +185,8 @@ DisplayDay
 	movwf	WL	;then into WL for division
 
 	movlw	10
-	movwf	CH
-	call	Div16x8	;
+	movwf	CH	;put 10 into CH
+	call	Div16x8	;divide remainder of days by 10
 	movwf	temp	;put remainder in temp
 	movf	WL,W	;put result in W
 	addlw	48	;add 30 to result to convert to ASCII
@@ -204,15 +225,15 @@ incDaysYears
 	btfsc	STATUS,C	;skip next instruction if Days0 does not cause carry flag
 	incf	Days1		;increment Days1 
 
-	;1 01101101 - 365 
-	;1 01101100 - 364
+		;1 01101110 - 366 
+		;1 01101101 - 365
 
 	movlw	%00000001 	;put 1 into w for 9th bit of day
 	subwf	Days1,W		;check if 9th bit of days is set
 	btfss	STATUS,Z	;if the bit is set skip next instruction
 	goto	notZero	
 
-	movlw	%01101100 	;put 
+	movlw	%01101101 	;put 
 	addwf	leapYr,W	;add leap year to 365, so if leap year it'll go to 365 days
 	subwf	Days0,W		;checks if day is at the max value - 364 or 365
 	btfsc	STATUS,Z	;if the byte isn't at max day skip next instruction
@@ -232,6 +253,9 @@ testLeap
 	btfss	STATUS,Z    ;test if the bits were 0
 	clrf	leapYr      ;if the bit wasnt zero then clear leapYr because it's not a leap year
 	return              
+
+
+
 
 
 
